d0fd5268934fdf681b2faacf4ab64f35f5ba95fd
Daniel Stevens <dan.stevens@oracle.com>
2012-06-15 12:22:58 +0100

    Initial commit

---
+++ TableLayoutSizingIssueTest/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace TableLayoutSizingIssueTest
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null)) {
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+			this.label1 = new System.Windows.Forms.Label();
+			this.comboField = new System.Windows.Forms.ComboBox();
+			this.label2 = new System.Windows.Forms.Label();
+			this.labelFieldSize = new System.Windows.Forms.Label();
+			this.tableLayoutPanel1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// tableLayoutPanel1
+			// 
+			this.tableLayoutPanel1.ColumnCount = 2;
+			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 100F));
+			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+			this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+			this.tableLayoutPanel1.Controls.Add(this.comboField, 1, 0);
+			this.tableLayoutPanel1.Controls.Add(this.label2, 0, 1);
+			this.tableLayoutPanel1.Controls.Add(this.labelFieldSize, 1, 1);
+			this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+			this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+			this.tableLayoutPanel1.RowCount = 2;
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+			this.tableLayoutPanel1.Size = new System.Drawing.Size(299, 60);
+			this.tableLayoutPanel1.TabIndex = 0;
+			this.tableLayoutPanel1.SizeChanged += new System.EventHandler(this.tableLayoutPanel1_SizeChanged);
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.label1.Location = new System.Drawing.Point(3, 0);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(94, 30);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "Field:";
+			this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+			// 
+			// comboField
+			// 
+			this.comboField.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.comboField.FormattingEnabled = true;
+			this.comboField.Location = new System.Drawing.Point(103, 5);
+			this.comboField.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
+			this.comboField.Name = "comboField";
+			this.comboField.Size = new System.Drawing.Size(193, 21);
+			this.comboField.TabIndex = 3;
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.label2.Location = new System.Drawing.Point(3, 30);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(94, 30);
+			this.label2.TabIndex = 2;
+			this.label2.Text = "Width of Field:";
+			this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+			// 
+			// labelFieldSize
+			// 
+			this.labelFieldSize.AutoSize = true;
+			this.labelFieldSize.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.labelFieldSize.Location = new System.Drawing.Point(103, 30);
+			this.labelFieldSize.Name = "labelFieldSize";
+			this.labelFieldSize.Size = new System.Drawing.Size(193, 30);
+			this.labelFieldSize.TabIndex = 4;
+			this.labelFieldSize.Text = "labelFieldSize";
+			this.labelFieldSize.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(299, 60);
+			this.Controls.Add(this.tableLayoutPanel1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.tableLayoutPanel1.ResumeLayout(false);
+			this.tableLayoutPanel1.PerformLayout();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.ComboBox comboField;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label labelFieldSize;
+	}
+}
+
---
+++ TableLayoutSizingIssueTest/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace TableLayoutSizingIssueTest
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+			labelFieldSize.Text = comboField.Size.Width.ToString();
+		}
+
+		private void tableLayoutPanel1_SizeChanged(object sender, EventArgs e)
+		{
+			labelFieldSize.Text = comboField.Size.Width.ToString();
+		}
+	}
+}
